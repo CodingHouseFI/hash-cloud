3ee3ab62da9ad63542cb0da914546676edee9526
AgileLabs <samer@agilelabs.com>
2015-07-07 15:58:44 -0700

    Data object

--- source/index.js
+++ source/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-angular.module('sif', ['firebase', 'ui.router']);
+var app = angular.module('sif', ['firebase', 'ui.router']);
+
+app.controller("mainCtrl", function($scope) {
+  $scope.tags = {
+    "#Angularjs": 40,
+    "#Emberjs": 20,
+    "#JavaScript": 80,
+    "#Reactjs": 10,
+    "#Backbone": 10,
+    "#MeanStack": 20,
+    "#Nodejs": 25,
+    "#Mongodb": 15,
+    "#Expressjs": 5
+  };
+});
